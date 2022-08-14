6338f216f7e4b75ac275c148ae08a168d3e3f495
technogleb <sinyakov@phystech.edu>
2022-08-14 20:16:03 +0300

    Add --watch_changes command into CLI
    * update docstring
    * refactor change_watcher

--- auto_summary/change_watcher.py
+++ auto_summary/change_watcher.py
@@ -11,6 +11,7 @@ from auto_summary import generate_summary
 
 
 logging.basicConfig(
+    filename='auto_summary.log',
     level=logging.INFO,
     format='%(asctime)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
@@ -31,14 +32,14 @@ class AutosummaryEventHandler(FileSystemEventHandler):
         src_path = Path(event.src_path).resolve()
         if (not event.event_type == 'modified') and (src_path.suffix == '.md'):
             generate_summary.main(self.path, wikilinks=self.wikilinks)
-            self.logger.info('Summary is updated')
+            self.logger.info(f'{src_path} is updated')
 
 
-def watch(path: Path, join=False):
-    event_handler = AutosummaryEventHandler(path)
+def watch(root: Path, join=False, **kwargs):
+    event_handler = AutosummaryEventHandler(root, **kwargs)
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.schedule(LoggingEventHandler(), path, recursive=True)
+    observer.schedule(event_handler, root, recursive=True)
+    observer.schedule(LoggingEventHandler(), root, recursive=True)
     observer.start()
     # set join=True, when you run this as standalone script. Running from vim, for example
     # needs main thread to be free, so join=False
--- cli.py
+++ cli.py
@@ -5,7 +5,17 @@ from auto_summary.generate_summary import main
 
 
 def run_parser():
-    parser = argparse.ArgumentParser('Provides CLI interface for generate_summary script')
+    parser = argparse.ArgumentParser(
+        'Provides CLI interface for Auto Summary\n'
+        'Syntax: python3 cli.py -r <markdown_root> [--wikilinks|watch_changes|h]\n'
+        'options:\n'
+        '-r                 Path to the root of you markdown filetree, auto_summary \n'
+        '                   recursively looks for markdown files starting from the root\n'
+        '--wikilinks        Whether to use wikilinks links format\n'
+        '--watch_changes    Whether to automatically update table of contents files,\n'
+        '                   when any file is moved/renamed/deleted/created.\n'
+        '                   Requires watchdog to be installed'
+    )
 
     parser.add_argument(
         '-r', '--root', type=str, required=True,
@@ -13,13 +23,26 @@ def run_parser():
     )
     parser.add_argument(
         '--wikilinks', action='store_true',
-        help='Whether to use wikilinks format or not. Default False.'
+        help='Whether to use wikilinks format or not. Default False'
+    )
+    parser.add_argument(
+        '--watch_changes', action='store_true',
+        help='Whether to automatically watch root for changes and run summarizer on each'
     )
 
     args = vars(parser.parse_args())
     args['root'] = Path(args['root']).resolve()
 
-    main(**args)
+    watch_changes = args.pop('watch_changes')
+    if watch_changes:
+        try:
+            from auto_summary.change_watcher import watch
+        except ImportError:
+            raise ImportError('watchdog is not found')
+        main(**args)  # initialize tables of content
+        watch(**args, join=True)
+    else:
+        main(**args)
 
 
 if __name__ == "__main__":
